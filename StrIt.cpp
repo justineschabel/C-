c26d1c9d9073dfca88333669ad66c8504928cb31
Justine Schabel <justine.schabel@gmail.com>
2017-10-14 12:06:42 -0700

    Update StrIt.cpp

--- StrIt.cpp
+++ StrIt.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
  using namespace std;
-
+//This program puts a string into a character array, this would allow manipulation of the string
 int main(){
   string str ("My String");
   string::iterator it;
   char strArray[str.length()];
   int index = 0;
-  for ( it = str.begin() ; it < str.end(); it++ ,index++)
-  {
-         cout << index << *it ;
+  for ( it = str.begin() ; it < str.end(); it++ ,index++){
+         index << *it ;
 	 strArray[index] = *it; 
-	cout<< "strArray: "<< strArray[index] << endl;
+	 cout<< "strArray: "<< strArray[index] << endl;
   }
 }
